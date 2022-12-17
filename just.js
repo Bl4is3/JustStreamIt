5bbfe5a67cf489ad903684a4a12fbeb54c827107
BL4IS3 <bl4is3@free.fr>
2022-12-17 04:22:56 +0100

    retrait de lien avec javascript, ajout de l'attribut 'data-id', image clicable sans lien

--- just.js
+++ just.js
@@ -28,7 +28,7 @@ function createModale(
     document.querySelector(".modal").style.display = "block";
     document.getElementById('titre').innerHTML += titre;
     document.getElementById('img_modale').src = image_url;
-    document.getElementById('genres').innerHTML += genres;
+    document.getElementById('genres').innerHTML = 'Genre: ' + genres;
     document.getElementById('date_published').innerHTML += date_published;
     document.getElementById('rated').innerHTML += rated;
     document.getElementById('imdb_score').innerHTML += imdb_score;
@@ -40,10 +40,9 @@ function createModale(
     document.getElementById('description').innerHTML += description;
 }
 
-function get_element_film(key, n) {
+function get_element_film(key, modale_creation = true) {
     url_film = url + key.toString();
     return fetch(url_film)
-        // interrogation de l'api pour l'id souhaité
         .then((resp) => resp.json())
         .then(function (movie) {
             var image_url = movie.image_url;
@@ -58,7 +57,7 @@ function get_element_film(key, n) {
             let countries = movie.countries;
             let avg_vote = movie.avg_vote;
             let description = movie.description;
-            if (n == 0) {
+            if (modale_creation) {
                 createModale(titre,
                     image_url,
                     genres,
@@ -73,7 +72,7 @@ function get_element_film(key, n) {
                     description)
             }
             else {
-                document.getElementById('oups').src = movie.image_url;
+                document.getElementById('top_film').src = movie.image_url;
                 document.getElementById('titre_best').innerHTML = movie.title
                 document.getElementById('resume_best').innerHTML += movie.description
             }
@@ -83,23 +82,22 @@ function get_element_film(key, n) {
         });
 }
 for (let [key, value] of categories) {
-    eval("var p_" + key + "=0;");
+    eval("var position_" + key + "=0;");
     afficherMasquer(key);
     fetch(value)
         .then((resp) => resp.json())
         .then(function (data) {
             let movies = data.results;
             if (key == 'Bests') {
-                document.getElementById('oups').src = movies[0].image_url;
-                get_element_film(movies[0].id, 1);
+                document.getElementById('top_film').src = movies[0].image_url;
+                get_element_film(movies[0].id, modale_creation = false);
                 //supression du 1er éléménet du tableau
                 movies.shift();
             }
             return movies.map(function (movie) {
-                const elt_href = document.getElementById(key).children;
-                const image = elt_href[movies.indexOf(movie)].children;
-                image[0].src = movie.image_url;
-                elt_href[movies.indexOf(movie)].href = `javascript: get_element_film(${movie.id},0);`;
+                const image = document.getElementById(key).children;
+                image[movies.indexOf(movie)].src = movie.image_url;
+                image[movies.indexOf(movie)].dataset.id = movie.id;  // utilisation de l'attaribut data- idela avec HTML5, recupérable en JS et en CSS
             })
         })
         .catch(function (error) {
@@ -107,82 +105,11 @@ for (let [key, value] of categories) {
         });
 }
 
-// function get_best_film(urlbestfilm) {
-//     // object-fit cover pour affichage
-//     return fetch(urlbestfilm)
-//         .then((resp) => resp.json())
-//         .then(function (data) {
-//             let movies = data.results;
-//             let div = document.getElementById('Best');
-//             let titre = createNode('h2');
-//             var btn = createNode('button');
-//             let img = createNode('img');
-//             // let span = createNode('span');
-//             var modal = createNode('div');
-//             var modal_content = createNode('div');
-//             var text_modal = createNode('p')
-//             var span_modal = createNode('span');
-//             titre.textContent += `${movies[0].title} `;
-//             img.src = movies[0].image_url;
-//             img.height = "300";
-//             img.width = '300';
-//             btn.id = `myBtn_${movies[0].id} `;
-
-//             modal.id = `myModal_${movies[0].id} `;
-//             modal.className = "modal";
-//             modal_content.className = 'modal-content';
-//             text_modal.textContent = `id:${movies[0].id} `;
-//             span_modal.className = "close";
-//             span_modal.textContent += 'X';
-//             append(div, titre);
-//             append(div, btn);
-//             append(btn, img);
-//             append(div, modal);
-//             append(modal, modal_content);
-//             append(modal_content, span_modal);
-//             append(modal_content, text_modal);
-//             var modal_locale = document.getElementById(`myModal_${movies[0].id} `);
-
-//             var btn_local = document.getElementById(`myBtn_${movies[0].id} `);
-
-//             btn_local.onclick = function () {
-//                 console.log('ca devrait etre Best');
-//                 modal_locale.style.display = "inline-block";
-
-//             }
-//             span_modal.onclick = function () {
-//                 modal_locale.style.display = "none";
-//             }
-//             window.onclick = function (event) {
-//                 if (event.target == modal_locale) {
-//                     modal_locale.style.display = "none";
-//                 }
-//             }
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         });
-// }
-
-// el = get_best_film(urlbestfilm);
-
 
 const span = document.querySelector('.close');
 span.addEventListener('click', function () {
     modale = document.querySelector('.modal');
     modale.style.display = "none";
-    document.getElementById('titre').innerHTML = "Titre: ";
-    document.getElementById('img_modale').src = "";
-    document.getElementById('genres').innerHTML = "genres: ";
-    document.getElementById('date_published').innerHTML = "date_published: ";
-    document.getElementById('rated').innerHTML = "rated: ";
-    document.getElementById('imdb_score').innerHTML = "imdb_score: ";
-    document.getElementById('directors').innerHTML = "directors: ";
-    document.getElementById('actors').innerHTML = "actors: ";
-    document.getElementById('duration').innerHTML = "duration: ";
-    document.getElementById('countries').innerHTML = "countries: ";
-    document.getElementById('avg_vote').innerHTML = "avg_vote: ";
-    document.getElementById('description').innerHTML = "description: ";
 
 });
 
@@ -190,13 +117,14 @@ const elts_left = document.querySelectorAll('.btn-left');
 elts_left.forEach(function (elt_left) {
     elt_left.addEventListener('click', function () {
         key = elt_left.previousElementSibling.childNodes[1].id;
-        eval("var p = p_" + key + ";");
-        if (p > -3)
-            p--;
+        eval("var position = position_" + key + ";");
+        if (position > -3) {
+            position--;
+        }
         element = document.getElementById(key)
-        element.style.transform = "translate(" + p * 200 + "px)";
+        element.style.transform = "translate(" + position * 200 + "px)";
         element.style.transition = "all 0.5s ease";
-        eval("p_" + key + " = p;");
+        eval("position_" + key + " = position;");
         afficherMasquer(key);
     });
 });
@@ -205,27 +133,36 @@ const elts_right = document.querySelectorAll('.btn-right');
 elts_right.forEach(function (elt_right) {
     elt_right.addEventListener('click', function () {
         key = elt_right.previousElementSibling.previousElementSibling.childNodes[1].id;
-        eval("var p = p_" + key + ";");
-        if (p < 0)
-            p++;
+        eval("var position = position_" + key + ";");
+        if (position < 0) {
+            position++;
+        }
         element = document.getElementById(key)
-        element.style.transform = "translate(" + p * 200 + "px)";
+        element.style.transform = "translate(" + position * 200 + "px)";
         element.style.transition = "all 0.5s ease";
-        eval("p_" + key + " = p;");
+        eval("position_" + key + " = position;");
         afficherMasquer(key);
+    });
+});
+
+const images_cliquable = document.querySelectorAll('.img');
+images_cliquable.forEach(function (image_cliquable) {
+    image_cliquable.addEventListener('click', function () {
+        get_element_film(image_cliquable.dataset.id)
+        console.log(image_cliquable.dataset.id)
     });
 });
 
 function afficherMasquer(key) {
     diva = document.getElementById(key).parentElement.parentElement;
-    eval("var p = p_" + key + ";");
+    eval("var position = position_" + key + ";");
     g = diva.querySelector(' .btn-left');
-    if (p == -3)
+    if (position == -3)
         g.style.visibility = "hidden";
     else
         g.style.visibility = "visible";
     d = diva.querySelector('.btn-right');
-    if (p == 0)
+    if (position == 0)
         d.style.visibility = "hidden";
     else
         d.style.visibility = "visible";
